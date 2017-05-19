5a3b4461ce53efc095baef17cdc7f0a558b90f68
Mark <mark.vanveen@gmail.com>
2017-05-19 17:45:12 +0200

    Change PRODUCT_OPERATIONS_MANAGEMENT key to new Add missing permission checks for loan product Disable fetch of enable product

--- src/services/portfolio/domain/permittable-group-ids.ts
+++ src/services/portfolio/domain/permittable-group-ids.ts
@@ -15,7 +15,7 @@
  */
 
 export class PortfolioPermittableGroupIds{
-  public static readonly PRODUCT_OPERATIONS_MANAGEMENT: string = 'portfolio__v1__products__enabling';
+  public static readonly PRODUCT_OPERATIONS_MANAGEMENT: string = 'portfolio__v1__products__enable';
   public static readonly PRODUCT_MANAGEMENT: string = 'portfolio__v1__products';
   public static readonly CASE_MANAGEMENT: string = 'portfolio__v1__case';
 }
