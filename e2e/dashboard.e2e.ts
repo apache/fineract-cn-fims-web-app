de7bbdf9caf07e64709fc3c05446768814bb8b70
Mark <mark.vanveen@gmail.com>
2017-05-24 20:19:11 +0200

    Fix unit tests not running after update

--- e2e/dashboard.e2e.ts
+++ e2e/dashboard.e2e.ts
@@ -1,18 +1,18 @@
-import { browser, element, by, $, protractor } from 'protractor/globals';
+import { browser, by, protractor, $, element, ProtractorExpectedConditions } from 'protractor';
 
 describe('basic e2e test with loading', function(): void {
-  let EC = protractor.ExpectedConditions;
+  let EC: ProtractorExpectedConditions = protractor.ExpectedConditions;
   describe('home', function(): void {
     browser.get('/');
-    it('should load home page', function(): void {
-      expect(browser.getTitle()).toBe('Covalent QuickStart');
+    it('should load quick access', function(): void {
+      expect(browser.getTitle()).toBe('Quick access');
       // Waits for the element 'td-loading' to not be present on the dom.
       browser.wait(EC.not(EC.presenceOf($('td-loading'))), 10000)
-      .then(() => {
+        .then(() => {
 
-        // checks if elements were rendered
-        expect(element(by.id('dashboard-favorites-card')).isPresent()).toBe(true);
-      });
+          // checks if elements were rendered
+          expect(element(by.id('dashboard-favorites-card')).isPresent()).toBe(true);
+        });
     });
   });
 });
