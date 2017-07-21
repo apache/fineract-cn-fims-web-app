c7261ffae686623cf4e0ddbaa3664dad9cd70d53
Mark <mark.vanveen@gmail.com>
2017-07-21 19:33:03 +0200

    Add currencies

--- src/services/currency/currency.service.ts
+++ src/services/currency/currency.service.ts
@@ -25,7 +25,10 @@ export class CurrencyService {
     { code: 'BZD', name: 'Belize Dollar', sign: '$', digits: 2 },
     { code: 'EUR', name: 'Euro', sign: '€', digits: 2 },
     { code: 'GMD', name: 'Gambian Dalasi', sign: 'D', digits: 2 },
+    { code: 'JMD', name: 'Jamaican Dollar', sign: '$', digits: 2 },
+    { code: 'MXN', name: 'Mexican Peso', sign: '$', digits: 2 },
     { code: 'USD', name: 'US Dollar', sign: '$', digits: 2 },
+    { code: 'TTD', name: 'Trinidad and Tobago Dollar', sign: '$', digits: 2 },
     { code: 'XCD', name: 'East Caribbean Dollar', sign: '$', digits: 2 }
   ];
 
